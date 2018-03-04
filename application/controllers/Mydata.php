e21e4680f89cdc3d8e7ca4ee343baa9893d68455
Bundit Sankhumpha <sankhumpha84@gmail.com>
2018-03-04 17:50:30 +0700

    add data vote

--- application/controllers/Mydata.php
+++ application/controllers/Mydata.php
@@ -20,6 +20,21 @@ class Mydata extends CI_Controller {
 		$this->load->view('mydata/index', $this);
 	}
 
+
+	public function mv($id)
+	{
+		$this->_login();
+		
+		$sql = 'SELECT *,(SELECT COUNT(vote_id) FROM vote WHERE vote.movie_id = movie.movie_id) as c FROM movie';
+		$this->movie = $this->db->query($sql)->result();
+
+		$sql = 'SELECT *,(SELECT COUNT(vote_id) FROM vote WHERE vote.member_id = member.id) as c FROM member JOIN vote ON member.id = vote.member_id WHERE vote.movie_id = ?';
+		$this->member = $this->db->query($sql, array(
+			$id
+		))->result();
+		$this->load->view('mydata/mv', $this);
+	}
+
 	public function login()
 	{
 		$this->load->view('mydata/login');
