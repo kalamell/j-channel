2e130f114132f6ea3578c4410013a63d3ba5f639
Bundit Sankhumpha <sankhumpha84@gmail.com>
2018-03-04 14:11:17 +0700

    update vote model

--- application/models/Vote_model.php
+++ application/models/Vote_model.php
@@ -10,10 +10,16 @@ class Vote_model extends CI_Model
 
 	public function setVote($id, $member_id)
 	{
-		$this->db->set('vote_date', 'NOW()', false)->insert('vote', array(
-			'member_id' => $member_id,
-			'movie_id' => $id
+		$rs = $this->db->query("SELECT * FROM `vote` WHERE vote_id = ? AND member_id = ? AND vote_timestamp <= CURRENT_TIMESTAMP - INTERVAL 60 MINUTE order by vote_timestamp DESC LIMIT 1", array(
+			$id, $member_id
 		));
+
+		if ($rs->num_rows() == 0) {
+			$this->db->set('vote_date', 'NOW()', false)->insert('vote', array(
+				'member_id' => $member_id,
+				'movie_id' => $id
+			));
+		}
 		return true;
 	}
 }
