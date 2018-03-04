a7ce7cfb12056f45ffe98761745a02d800a694c8
Bundit Sankhumpha <sankhumpha84@gmail.com>
2018-03-04 12:31:28 +0700

    add movie

---
+++ application/views/movie/t3.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta property="og:url"           content="" />
+  	<meta property="og:type"          content="website" />
+  	<meta property="og:title"         content="" />
+  	<meta property="og:description"   content="" />
+  	<meta property="og:image"         content="" />
+	<title>10th Okinawa International Movie Festival</title>
+	<link rel="stylesheet" href="assets/style/css/main.css">
+</head>
+<body>
+	<div class="wrapper movie">
+		<div class="container">
+			<div class="header">
+				<div class="sub-header h-one">
+					<a href="index.html"><img src="assets/images/logo.jpg" alt=""></a>
+				</div>
+				<div class="sub-header h-two">
+					<p>ลุ้นบินลัดฟ้า ร่วมสัมผัสประสบการณ์เกาะสวรรค์และเทศกาลภาพยนตร์นานาชาติโอกินาว่า<br/>ถึงประเทศญี่ปุ่น เพียงร่วมโหวตภาพยนตร์ที่คุณชื่นชอบ<br/>เพื่อเป็นตัวแทนภาพยนตร์ไทยในงานภาพยนตร์นานาชาติโอกินาว่า</p>
+					<img src="assets/images/headcenter.png" alt="">
+				</div>
+				<div class="sub-header h-three">
+					<img src="assets/images/oversize.jpg" alt="">
+					<p>T3 โอเวอร์ไซส์ ทลายพุง</p>
+				</div>
+			</div>
+			<div class="trailer">
+				<iframe width="400" height="225" src="https://www.youtube.com/embed/2PwWoBSYkh4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+				<div class="sub-trailer">
+					<div class="detail short-info">
+						<p class="ovs">ค่าย : ที โมเมนต์ (MONO)<br/>
+						เผยแพร่ :ที โมเมนต์ (MONO)<br/>
+						ฉาย : 23 มีนาคม 2560
+						</p>
+					</div>
+					<div class="detail vote-btn">
+						<a href="">
+							<img src="assets/images/btn.png" alt="">
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="info">
+				<div class="container">
+					<p>ข้อมูลเกี่ยวกับภาพยนตร์</p>
+					<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;โอเวอร์ไซส์ ทลายพุง (อังกฤษ: Oversize) เป็นภาพยนตร์ไทยแนวตลก ที่ออกฉายเมื่อวันที่ 23 มีนาคม พ.ศ. 2560  นำแสดงโดย ปราโมทย์ ปาทาน , ณัฐจารี หรเวชกุล และ ศุภวิชญ์ มีเปรมวัฒนา เป็นภาพยนตร์เรื่องแรกของค่าย ที โมเมนต์</p>
+					<p>เรื่องย่อ</p>
+					<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4 ตำรวจอ้วนไซส์เกิน 100 กิโล ประจำสถานีตำรวจภูธรสะพานชัน ผู้พลาดการจับโจรปล้นทองด้วยเหตุที่วิ่งไม่ทันเพราะอ้วนเกินไป ทำให้ทั้ง 4 โดนลงโทษจากผู้กำกับสุดเข้มผู้ไม่ยอมให้ภาพพจน์ตำรวจไทยเสื่อมเสีย ได้เคี่ยวเข็ญให้ทั้ง 4 ต้องฝึกโหดเพื่อลดน้ำหนัก ทั้งออกกำลังกายและควบคุมน้ำหนักอย่างเครงคัด ทั้งที่ใจทั้ง 4 คิดถึงแต่หมูกระทะอันคุ้นเคย และแล้วเรื่องท้าทายก็มาเยือนทั้ง 4 สหาย เมื่อรถขนเงินของธนาคารดังถูกปล้นหายไปกลางวันแสกๆ ทั้ง 4 จึงต้องแบ่งเวลาทั้งสืบคดีและยังอยู่ในช่วงวัดผลการคุมน้ำหนักของผู้กำกับสุดเฮี้ยบอีกด้วย และเมื่อเวลา 3 เดือนของการวัดผลใกล้เข้ามา คดีที่ค้างคาก็ยังไม่คลี่คลาย จ่าทั้ง 4 จะทำอย่างไรเพื่อกอบกู้ศักดิ์ศรีเพื่อให้เป็นที่ยอมรับอีกครั้ง</p>
+				</div>
+			</div>
+			<div class="sponsor">
+				<div class="container">
+					<ul>
+						<li><a href=""><img src="assets/images/contentth.png" alt=""></a></li>
+						<li><a href=""><img src="assets/images/culture.png" alt=""></a></li>
+						<li><a href=""><img src="assets/images/mono29.png" alt=""></a></li>
+						<li><a href=""><img src="assets/images/mthai.png" alt=""></a></li>
+						<li><a href=""><img src="assets/images/jchannel.png" alt=""></a></li>
+						<li><a href=""><img src="assets/images/yoshimoto.png" alt=""></a></li>
+						<li><a href=""><img src="assets/images/mcip.png" alt=""></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="footer">
+				<div class="container">
+					<img src="assets/images/footer.jpg" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ application/views/movie/t4.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta property="og:url"           content="" />
+  	<meta property="og:type"          content="website" />
+  	<meta property="og:title"         content="" />
+  	<meta property="og:description"   content="" />
+  	<meta property="og:image"         content="" />
+	<title>10th Okinawa International Movie Festival</title>
+	<link rel="stylesheet" href="assets/style/css/main.css">
+</head>
+<body>
+	<div class="wrapper movie">
+		<div class="container">
+			<div class="header">
+				<div class="sub-header h-one">
+					<a href="index.html"><img src="assets/images/logo.jpg" alt=""></a>
+				</div>
+				<div class="sub-header h-two">
+					<p>ลุ้นบินลัดฟ้า ร่วมสัมผัสประสบการณ์เกาะสวรรค์และเทศกาลภาพยนตร์นานาชาติโอกินาว่า<br/>ถึงประเทศญี่ปุ่น เพียงร่วมโหวตภาพยนตร์ที่คุณชื่นชอบ<br/>เพื่อเป็นตัวแทนภาพยนตร์ไทยในงานภาพยนตร์นานาชาติโอกินาว่า</p>
+					<img src="assets/images/headcenter.png" alt="">
+				</div>
+				<div class="sub-header h-three">
+					<img src="assets/images/9.jpg" alt="">
+					<p>T4 9 ศาสตรา</p>
+				</div>
+			</div>
+			<div class="trailer">
+				<iframe width="400" height="225" src="https://www.youtube.com/embed/mUCiOQTPYT8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+				<div class="sub-trailer">
+					<div class="detail short-info">
+						<p>ค่าย : เอ็กซ์ฟอร์แมท ฟิล์มส์<br/>
+						เผยแพร่ : เอ็ม พิคเจอร์ส<br/>
+						ฉาย : 11 มกราคม 2561<br/>
+						ความยาว : 102 นาที
+						</p>
+					</div>
+					<div class="detail vote-btn">
+						<a href="">
+							<img src="assets/images/btn.png" alt="">
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="info">
+				<div class="container">
+					<p>ข้อมูลเกี่ยวกับภาพยนตร์</p>
+					<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;9 ศาสตรา เป็นภาพยนตร์แอนิเมชัน เนื้อหาว่าด้วยแฟนตาซีผสมผสานกับมวยไทย อำนวยการสร้างโดยบริษัท เอ็กซ์ฟอร์แมท ฟิล์มส์ และจัดจำหน่ายในประเทศไทยโดย บริษัท เอ็ม พิคเจอร์ส เอ็นเตอร์เทนเม้นท์ จำกัด (มหาชน) ซึ่งใช้เวลาในการสร้างถึง 4 ปีเต็ม และได้ออกฉายตรงกับสัปดาห์ วันเด็กแห่งชาติ เมื่อวันที่ 11 มกราคม พ.ศ. 2561</p>
+					<p>เรื่องย่อ</p>
+					<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;9 ศาสตรา เป็นเรื่องราวการผจญภัยของเด็กหนุ่มคนหนึ่ง ที่ชะตาลิขิตให้มากอบกู้อาณาจักรรามเทพนคร แผ่นดินปิตุภูมิของเขาให้รอดพ้นอำนาจทมิฬของยักษา ผู้ก่อความทุกข์เข็ญให้ประชาราษฎร์ เป็นการเดินทางที่ต้องอาศัยทั้งโชคชะตาและวิชายุทธอย่างมีคุณธรรมอ๊อด ชายหนุ่มแห่งโพ้นทะเล คือ ผู้ที่ถูกลิขิตมาให้รับภารกิจอันยิ่งใหญ่ในการกอบกู้อาณาจักร พร้อมกับพลพรรคเพื่อนพ้อง ที่มีทั้งอาวุธและยุทธวิธีตามวิถีตะวันออก ไม่ว่าจะเป็น “เสี่ยวหลาน”โจรสลัดอากาศชาวจีน เธอเป็นสาวงามนักแม่นปืนจากกองเรือเหาะทะยานเมฆ พร้อมด้วยลิงทโมน นามว่า วาตะ รวมถึง อสูรสีชาดยักษ์สีแดงร่างใหญ่ใจดี ส่วนอ๊อดนั้นได้ร่ำเรียนศิลปะการต่อสู้มวยไทย ที่เคยหายสาบสูญจากครูมวยอันดับหนึ่งของแผ่นดิน และได้ฝึกฝนเคี่ยวกรำอย่างหนักมาโดยตลอด เพื่อเตรียมต่อสู้กับหมู่มวลอธรรม</p>
+				</div>
+			</div>
+			<div class="sponsor">
+				<div class="container">
+					<ul>
+						<li><a href=""><img src="assets/images/contentth.png" alt=""></a></li>
+						<li><a href=""><img src="assets/images/culture.png" alt=""></a></li>
+						<li><a href=""><img src="assets/images/mono29.png" alt=""></a></li>
+						<li><a href=""><img src="assets/images/mthai.png" alt=""></a></li>
+						<li><a href=""><img src="assets/images/jchannel.png" alt=""></a></li>
+						<li><a href=""><img src="assets/images/yoshimoto.png" alt=""></a></li>
+						<li><a href=""><img src="assets/images/mcip.png" alt=""></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="footer">
+				<div class="container">
+					<img src="assets/images/footer.jpg" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
