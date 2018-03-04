ea30e24eb9ae0a9e09bc2fdbe7ac4364b6d254ca
Bundit Sankhumpha <sankhumpha84@gmail.com>
2018-03-04 13:58:21 +0700

    update model member

--- application/controllers/Vote.php
+++ application/controllers/Vote.php
@@ -14,10 +14,8 @@ class Vote extends CI_Controller {
 		$this->session->set_userdata('current_url', current_url());
 		if ($this->facebook->is_authenticated()) {
 			echo 'ok';
-			if ($this->session->userdata('id')) {
-				$this->vote->setVote($id, $this->session->userdata('id'));
-			} 
-			redirect($this->session->userdata('current_url'));
+			
+			//redirect($this->session->userdata('current_url'));
 		} else {
 			redirect($this->facebook->login_url());
 		}
