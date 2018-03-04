ae1ce8f29ad7cf4f3f42b310911408a8f304597c
Bundit Sankhumpha <sankhumpha84@gmail.com>
2018-03-04 12:30:30 +0700

    add main new

---
+++ application/views/main.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta property="og:url"           content="" />
+  	<meta property="og:type"          content="website" />
+  	<meta property="og:title"         content="" />
+  	<meta property="og:description"   content="" />
+  	<meta property="og:image"         content="" />
+	<title>10th Okinawa International Movie Festival</title>
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/style/css/main.css">
+	<?php $this->load->view('ga');?>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="container">
+			<div class="header">
+				<div class="sub-header h-one">
+					<a href="<?php echo site_url();?>"><img src="<?php echo base_url();?>assets/images/logo.jpg" alt=""></a>
+				</div>
+				<div class="sub-header h-two">
+					<p>ลุ้นบินลัดฟ้า ร่วมสัมผัสประสบการณ์เกาะสวรรค์และเทศกาลภาพยนตร์นานาชาติโอกินาว่า<br/>ถึงประเทศญี่ปุ่น เพียงร่วมโหวตภาพยนตร์ที่คุณชื่นชอบ<br/>เพื่อเป็นตัวแทนภาพยนตร์ไทยในงานภาพยนตร์นานาชาติโอกินาว่า</p>
+					<img src="<?php echo base_url();?>assets/images/headcenter.png" alt="">
+				</div>
+				<div class="sub-header h-three">
+					<img src="<?php echo base_url();?>assets/images/headright.png" alt="">
+					<p class="big">เทศกาลภาพยนตร์นานาชาติโอกินาว่า ครั้งที่ 10</p>
+					<p class="small">วันที่ 19 - 22 เมษายน 2561<br/>
+					เมืองโอกินาว่า ประเทศญี่ปุ่น</p>
+					<img src="<?php echo base_url();?>assets/images/toon.png" alt="">
+				</div>
+			</div>
+			<div class="content">
+				<div class="container">
+					<ul>
+						<li class="sub-content c-one">
+							<a href="<?php echo site_url('movie/id/1');?>">
+								<img src="<?php echo base_url();?>assets/images/genius.jpg" alt="">
+								<p>T1 ฉลาดเกมส์โกง</p>
+							</a>
+						</li>
+						<li class="sub-content c-two">
+							<a href="<?php echo site_url('movie/id/2');?>">
+								<img src="<?php echo base_url();?>assets/images/dietmr.jpg" alt="">
+								<p>T2 Die Tomorrow</p>
+							</a>
+						</li>
+						<li class="sub-content c-three">
+							<a href="<?php echo site_url('movie/id/3');?>">
+								<img src="<?php echo base_url();?>assets/images/oversize.jpg" alt="">
+								<p>T3 โอเวอร์ไซส์ ทลายพุง</p>
+							</a>
+						</li>
+						<li class="sub-content c-four">
+							<a href="<?php echo site_url('movie/id/4');?>">
+								<img src="<?php echo base_url();?>assets/images/9.jpg" alt="">
+								<p>T4 9 ศาสตรา</p>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="condition">
+				<div class="container">
+					<p>เงื่อนไขการร่วมกิจกรรม</p>
+					<p>- ผู้โชคดีจะได้รับแพ็คเกจทัวร์พร้อมที่พักไปชมเทศกาลภาพยนตร์นานาชาติโอกินาว่า ณ จังหวัดโอกินาว่า ประเทศญี่ปุ่น กับบริษัท โยชิโมโต้ เอ็นเทอร์เทนเมนท์ (ไทยแลนด์) จำกัด 2 รางวัล รางวัลละ 1 ที่นั่ง</p>
+					<p>- ร่วมโหวตได้ทาง http://j-channel.jp/th/oimf/  ตั้งแต่วันที่ 1 – 15 มีนาคม 2561 (รอคอนเฟิมนะ) (ปิดรับผลโหวตเวลา 24.00 น. ของวันที่ 15 มีนาคม 2561</p>
+					<p>- ผู้ร่วมกิจกรรมโหวต สามารถโหวตได้ชั่วโมงละ 1 โหวตเท่านั้น</p>
+					<p>- ประกาศรายชื่อผู้โชคดีทาง http://j-channel.jp/th/oimf/ ในวันที่ xxx</p>
+					<p>- ผู้รับรางวัลจะต้องเป็นผู้ที่ทายผลตัวแทนภาพยนตร์ของประเทศไทยที่ถูกต้องเท่านั้น</p>
+					<p>- ผลการตัดสินของคณะกรรมการถือเป็นที่สิ้นสุด</p>
+				</div>
+			</div>
+			<div class="sponsor">
+				<div class="container">
+					<ul>
+						<li><a href=""><img src="<?php echo base_url();?>assets/images/contentth.png" alt=""></a></li>
+						<li><a href=""><img src="<?php echo base_url();?>assets/images/culture.png" alt=""></a></li>
+						<li><a href=""><img src="<?php echo base_url();?>assets/images/mono29.png" alt=""></a></li>
+						<li><a href=""><img src="<?php echo base_url();?>assets/images/mthai.png" alt=""></a></li>
+						<li><a href=""><img src="<?php echo base_url();?>assets/images/jchannel.png" alt=""></a></li>
+						<li><a href=""><img src="<?php echo base_url();?>assets/images/yoshimoto.png" alt=""></a></li>
+						<li><a href=""><img src="<?php echo base_url();?>assets/images/mcip.png" alt=""></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="footer">
+				<div class="container">
+					<img src="<?php echo base_url();?>assets/images/footer.jpg" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
