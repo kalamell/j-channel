eea711c053f661b5232c36adefd4ef2e1887cc0e
Bundit Sankhumpha <sankhumpha84@gmail.com>
2018-03-04 17:39:32 +0700

    update mydata /

--- application/views/mydata/index.php
+++ application/views/mydata/index.php
@@ -9,13 +9,21 @@
 
 	<div class="container-fluid">
 		<div class='row'>
+			<div class='col-md-12'>
+				<h2 class="page-header">ข้อมูลการโหวต</h2>
+			</div>
+
 			<section class="col-md-4">
 				<div class="list-group">
 				  <a href="#" class="list-group-item">
 				    หน้าหลักแคมเปญ
 				  </a>
 				  <?php foreach($movie as $m):?>
-				  <a href="<?php echo site_url('mydata/mv/'.$m->movie_id);?>" class="list-group-item"><?php echo $m->movie_name;?></a>
+				  <a href="<?php echo site_url('mydata/mv/'.$m->movie_id);?>" class="list-group-item">
+				  	<span class="badge"><?php echo $m->c;?> โหลต</span>
+				  	<?php echo $m->movie_name;?>
+				  		
+				  </a>
 				<?php endforeach;?>
 				</div>
 				
