c21f3687836c239eccf6c41d96330575ef4459a0
Bundit Sankhumpha <sankhumpha84@gmail.com>
2018-03-04 14:33:29 +0700

    update vote model fixed first vote

--- application/models/Vote_model.php
+++ application/models/Vote_model.php
@@ -10,15 +10,31 @@ class Vote_model extends CI_Model
 
 	public function setVote($id, $member_id)
 	{
-		$rs = $this->db->query("SELECT * FROM `vote` WHERE vote_id = ? AND member_id = ? AND vote_timestamp <= CURRENT_TIMESTAMP - INTERVAL 60 MINUTE order by vote_timestamp DESC LIMIT 1", array(
-			$id, $member_id
-		));
+		$check = $this->db->where(array(
+			'member_id' => $member_id,
+			'movie_id' => $movie_id
+		)))->get('vote');
+		
+		if ($check->num_rows() == 0) {
+			if ($rs->num_rows() > 0) {
+				$this->db->set('vote_date', 'NOW()', false)->insert('vote', array(
+					'member_id' => $member_id,
+					'movie_id' => $id
+				));
+			}
+			
+		} else {
 
-		if ($rs->num_rows() > 0) {
-			$this->db->set('vote_date', 'NOW()', false)->insert('vote', array(
-				'member_id' => $member_id,
-				'movie_id' => $id
+			$rs = $this->db->query("SELECT * FROM `vote` WHERE vote_id = ? AND member_id = ? AND vote_timestamp <= CURRENT_TIMESTAMP - INTERVAL 60 MINUTE order by vote_timestamp DESC LIMIT 1", array(
+				$id, $member_id
 			));
+
+			if ($rs->num_rows() > 0) {
+				$this->db->set('vote_date', 'NOW()', false)->insert('vote', array(
+					'member_id' => $member_id,
+					'movie_id' => $id
+				));
+			}
 		}
 		return true;
 	}
