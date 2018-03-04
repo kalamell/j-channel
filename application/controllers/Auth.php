f1b2c595ef57b2ea9585f014fbacc76cc6ee60b2
Bundit Sankhumpha <sankhumpha84@gmail.com>
2018-03-04 13:53:37 +0700

    update auth

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -11,11 +11,27 @@ class Auth extends CI_Controller {
 
 	public function login()
 	{
-		print_r($this->facebook->object());
+		
 
+		
 		$userProfile = $this->facebook->request('get', '/me?fields=id,first_name,last_name,email,gender,locale,picture');
 
-		print_r($userProfile);
+            // Preparing data for database insertion
+        $userData['provider'] = 'facebook';
+        $userData['uid'] = $userProfile['id'];
+        $userData['first_name'] = $userProfile['first_name'];
+        $userData['last_name'] = $userProfile['last_name'];
+        $userData['email'] = $userProfile['email'];
+        $userData['gender'] = $userProfile['gender'];
+        $userData['locale'] = $userProfile['locale'];
+        $userData['profile_url'] = 'https://www.facebook.com/'.$userProfile['id'];
+        $userData['picture_url'] = $userProfile['picture']['data']['url'];
+
+        // Insert or update user data
+        $userID = $this->member->checkUser($userData);
+
+        redirect($this->session->userdata('current_url'));
+
 
 		/*print_r($this->session->userdata());
 
