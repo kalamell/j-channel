66c8e7e493da8780f2ddf428b19a694c335e7a06
Bundit Sankhumpha <sankhumpha84@gmail.com>
2018-03-08 22:41:09 +0700

    update load helper

--- application/helpers/fn_helper.php
+++ application/helpers/fn_helper.php
@@ -3,7 +3,7 @@
 function getVote($member_id)
 {
 	$ci =& get_instance();
-	$sql = "SELECT *,(SELECT COUNT(vote_id) FROM vote WHERE vote.movie_id = movie.movie_id AND vote.member_id = ?) FROM movie";
+	$sql = "SELECT *,(SELECT COUNT(vote_id) FROM vote WHERE vote.movie_id = movie.movie_id AND vote.member_id = ?) as c FROM movie";
 	$rs = $ci->db->query($sql, array($member_id));
 
 	return $rs->result();
